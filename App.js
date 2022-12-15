45b843f4c95b1ebab7a96ef08369acb0cca299f2
James Kim <jameskim@Jamess-MacBook-Pro.local>
2022-12-15 17:21:23 +0900

    gesture & animation setup

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useMemo, useCallback} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {
   GestureHandlerRootView,
@@ -8,17 +8,62 @@ import {
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
-  withSpring,
+  withTiming,
+  Easing,
 } from 'react-native-reanimated';
 
 const Slider = () => {
+  const sliderContainerHeight = 200;
+  const sliderToggleHeight = 40;
+
+  const sliderSteps = useMemo(() => {
+    return {
+      0: 0,
+      // 1: sliderContainerHeight * 0.125,
+      2: (sliderContainerHeight - sliderToggleHeight) * 0.25,
+      // 3: sliderContainerHeight * 0.375,
+      4: sliderContainerHeight * 0.5 - sliderToggleHeight / 2, // 80
+      // 5: sliderContainerHeight * 0.625,
+      6: (sliderContainerHeight - sliderToggleHeight) * 0.75,
+      // 7: sliderContainerHeight * 0.875,
+      8: sliderContainerHeight - sliderToggleHeight, // 160
+    };
+  }, [sliderContainerHeight]);
+  // sliderSteps key
+  // 0 -  1  - 2 -  3  - 4 -  5  - 6 -  7  - 8
+  // sliderSteps limits
+  // 0 - 0.5 - 1 - 1.5 - 2 - 2.5 - 3 - 3.5 - 4
+  // sliderContainerHeight
+  // 0 - 25 - 50 - 75 - 100 - 125 - 150 - 175 - 200
+  // 200 / 8 = 25
+  // stepVal = 25
+  // ex) offset = 12, 12 <= 25 ? use that key val for
+
+  // const gestureEndOffsetY = useSharedValue(0);
+
+  // const calcSliderStep = offsetY => {
+  //   // compare offset to sliderSteps values
+  //   for (const [k, v] of Object.entries(sliderSteps)) {
+  //     if (offsetY <= v) {
+  //       console.log('This is the step', k, v);
+  //       gestureEndOffsetY.value = v;
+  //     }
+  //   }
+  //   return;
+  // };
+
   const isPressed = useSharedValue(false);
   const offset = useSharedValue({x: 0, y: 0});
   const animatedStyles = useAnimatedStyle(() => {
     return {
       transform: [
         {translateX: offset.value.x},
-        {translateY: withSpring(offset.value.y)},
+        {
+          translateY: withTiming(offset.value.y, {
+            duration: 0,
+            easing: Easing.inOut,
+          }),
+        },
       ],
       backgroundColor: isPressed.value ? '#f2f2f2' : '#fff',
     };
@@ -31,25 +76,67 @@ const Slider = () => {
       isPressed.value = true;
     })
     .onUpdate(e => {
+      console.log(e.translationY + start.value.y);
+
+      const toggleOffsetY = e.translationY + start.value.y;
+      const maxLimit = (sliderContainerHeight - sliderToggleHeight) * -1;
+
+      if (toggleOffsetY > 0) {
+        offset.value = {
+          x: 0,
+          y: 0,
+        };
+      } else if (toggleOffsetY < maxLimit) {
+        offset.value = {
+          x: 0,
+          y: maxLimit,
+        };
+      } else {
+        offset.value = {
+          x: 0,
+          y: toggleOffsetY,
+        };
+      }
+    })
+    .onEnd(e => {})
+    .onFinalize(e => {
+      const finalOffsetY = (e.translationY + start.value.y) * -1;
+      console.log(finalOffsetY);
+      // calculate which sliderStep to move to
+      // compare offset to sliderSteps values
+      let gestureEndOffsetY;
+      if (finalOffsetY > sliderContainerHeight - sliderToggleHeight) {
+        gestureEndOffsetY = sliderContainerHeight - sliderToggleHeight;
+      }
+      for (const [k, v] of Object.entries(sliderSteps)) {
+        if (finalOffsetY <= v) {
+          console.log('This is the step', k, v);
+          gestureEndOffsetY = v;
+          break;
+        }
+      }
       offset.value = {
         x: 0,
-        y: e.translationY + start.value.y,
+        y: gestureEndOffsetY * -1,
       };
-    })
-    .onEnd(() => {
       start.value = {
         x: 0,
         y: offset.value.y,
       };
-    })
-    .onFinalize(() => {
       isPressed.value = false;
     });
 
   return (
-    <Animated.View style={styles.sliderContainer}>
+    <Animated.View
+      style={[styles.sliderContainer, {height: sliderContainerHeight}]}>
       <GestureDetector gesture={gesture}>
-        <Animated.View style={[styles.sliderToggle, animatedStyles]} />
+        <Animated.View
+          style={[
+            styles.sliderToggle,
+            {height: sliderToggleHeight},
+            animatedStyles,
+          ]}
+        />
       </GestureDetector>
     </Animated.View>
   );
@@ -83,7 +170,6 @@ const styles = StyleSheet.create({
   },
   sliderContainer: {
     // flex: 1,
-    height: 200,
     width: 50,
     backgroundColor: '#D0D0D0',
     borderRadius: 4,
@@ -93,7 +179,6 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     flex: 1,
-    height: 20,
     width: 50,
     borderRadius: 4,
     borderWidth: 1,
